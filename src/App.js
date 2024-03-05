a76a031da5d60784f4dc5111e1b73e0898a96aa9
Sean Jimwell Buclatin <105427349+seanj12c@users.noreply.github.com>
2024-03-05 12:33:11 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Admins from "./components/Admins";
 import Contacts from "./components/Contacts";
 import Facilities from "./components/Facilities";
 import Features from "./components/Features";
 import Hero from "./components/Hero";
 import { Navbar } from "./components/Navbar";
+import Footer from "../src/components/Footer"
 
 function App() {
   return (
@@ -11,8 +13,10 @@ function App() {
       <Navbar/>
       <Hero/>
       <Facilities/>
+      <Admins/>
       <Features/>
       <Contacts/>
+      <Footer/>
     </div>
   );
 }
