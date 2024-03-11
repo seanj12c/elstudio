0b0873eac27897c227ab1a695f7f6bba3d91d8f7
Sean Jimwell Buclatin <105427349+seanj12c@users.noreply.github.com>
2024-03-11 11:39:11 +0800

    change the image of hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
       className="hero min-h-screen"
       style={{
         backgroundImage:
-          "url(https://images.unsplash.com/photo-1486691541334-8cbe8b4ea413?q=80&w=2372&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
+          "url(https://images.unsplash.com/photo-1554579859-b14939c01ec6?q=80&w=2146&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
       }}
     >
       <div className="hero-overlay bg-opacity-60"></div>
